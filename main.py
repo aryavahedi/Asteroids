13df76068f1b16a6aede613fb4b89b5c0f309369
Arya Vahedi <39989822+aryavahedi@users.noreply.github.com>
2026-01-06 14:14:52 +0100

    Player is comeplete

--- main.py
+++ main.py
@@ -3,19 +3,27 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
 
+
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
+        updatable.update(dt)
+        for drawy in drawable:
+            drawy.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000.0
     print("Starting Asteroids with pygame version: 2.6.1")
